532664f8690596bccc1a7da3716b15c2b61c29c5
hoangvanhuanvn <hoangvanhuanvn@84e44491-099a-6a52-a08b-60b0bcb5c27e>
2011-08-25 16:10:11 +0000

    Test generate random data

---
+++ Experiments/NHibernate/GoogleCode/SourceCode/GoogleCode.GenerateRandomData/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HibernatingRhinos.Profiler.Appender.NHibernate;
+using GDNET.Extensions.NHibernateImpl;
+using GoogleCode.Data.Mapping;
+
+namespace GoogleCode.GenerateRandomData
+{
+    class Program
+    {
+        private const string DefaultCfgFile = "hibernate.cfg.xml";
+
+        static void Main(string[] args)
+        {
+            NHibernateProfiler.Initialize();
+            NHSessionManager.Initialize(MappingUtil.GetHbmMapping(), DefaultCfgFile);
+
+            using (var session = NHSessionManager.OpenSession())
+            {
+                using (var transaction = session.BeginTransaction())
+                {
+                    RandomData.Create(session);
+                    transaction.Commit();
+                }
+            }
+        }
+    }
+}
