0497f328779a31783d4e33e81bde0eeeb6a9af1c
hoangvanhuanvn <hoangvanhuanvn@84e44491-099a-6a52-a08b-60b0bcb5c27e>
2011-08-27 16:45:58 +0000

    Add new extension methods

--- trunk/Experiments/Common/GDNET.Common/Extensions/DateTimeExtensions.cs
+++ trunk/Experiments/Common/GDNET.Common/Extensions/DateTimeExtensions.cs
@@ -9,22 +9,45 @@ namespace GDNET.Common.Extensions
     public static class DateTimeExtensions
     {
         /// <summary>
-        /// Parse a string to date time.
+        /// Parse a string to dateValue time.
         /// </summary>
         /// <param name="stringOfDate">String value to be parsed.</param>
-        /// <param name="defaultValue">Default value to return in case of any error.</param>
+        /// <param name="defaultValue">Default value to return in case of any errorException.</param>
         /// <returns></returns>
         public static DateTime Parse(this string stringOfDate, DateTime defaultValue)
         {
+            return stringOfDate.Parse(defaultValue, defaultValue);
+        }
+
+        /// <summary>
+        /// Parse a string to date time.
+        /// </summary>
+        /// <param name="stringOfDate">String value to be parsed.</param>
+        /// <param name="defaultValue">Default value to return in case of any errorException.</param>
+        /// <returns></returns>
+        public static DateTime Parse(this string stringOfDate, DateTime defaultValue, DateTime minDate)
+        {
+            if (defaultValue < minDate)
+            {
+                defaultValue = minDate;
+            }
+            DateTime dateValue = defaultValue;
+
             try
             {
-                return DateTime.Parse(stringOfDate.Trim());
+                dateValue = DateTime.Parse(stringOfDate.TrimWithHtmlSpaces().Trim());
+
+                if (dateValue < minDate)
+                {
+                    dateValue = minDate;
+                }
             }
             catch (Exception ex)
             {
                 Debug.WriteLine(ex);
-                return defaultValue;
             }
+
+            return dateValue;
         }
     }
 }
