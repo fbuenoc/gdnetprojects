c684a6b97eabdfdac75960f250285d646a5c8bd2
hoangvanhuanvn@gmail.com <hoangvanhuanvn@gmail.com@84e44491-099a-6a52-a08b-60b0bcb5c27e>
2012-09-23 04:34:49 +0000

    V0.2

---
+++ trunk/WebFramework/GDNET.FrameworkInfrastructure/Controllers/SearchController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Web.Mvc;
+using GDNET.Domain.Content;
+using GDNET.Domain.Repositories;
+using GDNET.FrameworkInfrastructure.Common.Base;
+using GDNET.FrameworkInfrastructure.Common.Extensions;
+using GDNET.FrameworkInfrastructure.Models.Content;
+using GDNET.FrameworkInfrastructure.Models.SearchModels;
+using GDNET.FrameworkInfrastructure.Services;
+
+namespace GDNET.FrameworkInfrastructure.Controllers
+{
+    public class SearchController : AbstractController
+    {
+        private const int DefaultPageSize = 10;
+        private const int FocusItemSize = 10;
+        private const string SearchBy = "by";
+        private const string SearchValue = "value";
+        private const string SearchAuthor = "author";
+
+        public ActionResult Index()
+        {
+            string searchBy = base.Request.Params[SearchBy];
+
+            switch (searchBy)
+            {
+                case SearchAuthor:
+                    return this.SearchByAuthor();
+                default:
+                    return base.RedirectToAction("Index", "Home");
+            }
+        }
+
+        private ActionResult SearchByAuthor()
+        {
+            string authorId = base.Request.Params[SearchValue];
+            var author = DomainRepositories.User.GetById(new Guid(authorId));
+            var authorModel = WebFrameworkServices.AccountModels.GetUpdateDetailsModel(author);
+
+            var topItems = DomainRepositories.ContentItem.GetTopWithActiveByAuthor(DefaultPageSize, author.Email);
+            var topModels = FrameworkExtensions.ConvertAll<ContentItemModel, ContentItem>(topItems, true);
+
+            var focusItems = DomainRepositories.ContentItem.GetTopWithActiveByViews(FocusItemSize);
+            var focusModels = FrameworkExtensions.ConvertAll<ContentItemModel, ContentItem>(focusItems, true);
+
+            SearchByAuthorModel model = new SearchByAuthorModel(SearchMode.Author)
+            {
+                NewItems = topModels,
+                FocusItems = focusModels,
+                AuthorModel = authorModel,
+            };
+
+            return base.View("SearchByAuthor", model);
+        }
+    }
+}
---
+++ trunk/WebFramework/GDNET.FrameworkInfrastructure/Models/SearchModels/SearchByAuthorModel.cs
@@ -0,0 +1,27 @@
+﻿using GDNET.FrameworkInfrastructure.Common.Models;
+using GDNET.FrameworkInfrastructure.Models.HomeModels;
+
+namespace GDNET.FrameworkInfrastructure.Models.SearchModels
+{
+    public class SearchByAuthorModel : IndexModel
+    {
+        public SearchByAuthorModel(SearchMode mode)
+            : base()
+        {
+            this.Mode = mode;
+            this.AuthorModel = new UpdateDetailsModel();
+        }
+
+        public SearchMode Mode
+        {
+            get;
+            set;
+        }
+
+        public UpdateDetailsModel AuthorModel
+        {
+            get;
+            set;
+        }
+    }
+}
