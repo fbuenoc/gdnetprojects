bd32b72ebfae7e58b35884fe4a5ee5c4b5486e4b
hoangvanhuanvn <hoangvanhuanvn@gmail.com>
2012-10-14 09:18:20 +0000

    JQuery extensions

---
+++ WebFramework/GDNET.Web/Extensions/JQueryConstants.cs
@@ -0,0 +1,25 @@
+﻿namespace GDNET.Web.Extensions
+{
+    public sealed class JQueryConstants
+    {
+        public const string Source = "source";
+        public const string Cache = "cache";
+        public const string Timeout = "timeout";
+        public const string Type = "type";
+        public const string Url = "url";
+        public const string Data = "data";
+        public const string BeforeSend = "beforeSend";
+        public const string Success = "success";
+        public const string Error = "error";
+
+        /// <summary>
+        /// The id of container element
+        /// </summary>
+        public const string Id = "id";
+
+        /// <summary>
+        /// The body of document.ready function
+        /// </summary>
+        public const string Body = "body";
+    }
+}
